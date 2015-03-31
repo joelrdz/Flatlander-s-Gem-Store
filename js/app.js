802c33314d047e080c25810cb111e5f1b953b002
Joel Rdz <joelrdz@outlook.com>
2015-03-31 00:03:28 -0600

    ng-repeat

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 (function(){
 	var app = angular.module('gemStore', []);
 
-	var gem = {
-		name: 'Azurite',
-		price: 110.50,
-		canPurchase: false,
-		soldOut: true
-	};
+	var gems = [
+		{ name: 'Azurite', price: 2.95 },
+		{ name: 'Bloodstone', price: 5.95 },
+		{ name: 'Zircon', price: 3.95 },
+	];
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 }) ();
